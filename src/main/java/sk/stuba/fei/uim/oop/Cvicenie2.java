337742e07ee0127e21707948f8d3d9f840428216
Matus-Zlacky <xzlacky@stuba.sk>
2021-03-16 15:33:51 +0100

    priklady 1

--- src/main/java/sk/stuba/fei/uim/oop/Cvicenie2.java
+++ src/main/java/sk/stuba/fei/uim/oop/Cvicenie2.java
@@ -3,12 +3,38 @@ package sk.stuba.fei.uim.oop;
 import java.util.Arrays;
 
 public class Cvicenie2 {
-    public static void main(String... args) {
-        vypis("text", 5, 3, 69, 7, 2, 69, 9, 14, 6);
-    }
+    public static void main(String[] args) {
+        uloha1();
+        int x = KeyboardInput.readInt("zadaj prve cislo ");
+        int y = KeyboardInput.readInt("zadaj prve cislo ");
+        System.out.println("Vysledok ulohy2 je: " + uloha2(x, y));
+
+  /*  char y = KeyboardInput.readChar("zadaj pismeno: ");
+    System.out.println(x+ " "+ y);
 
     public static void vypis( String text, int... pole) {
         System.out.println(text);
         System.out.println(Arrays.toString(pole));
+    }*/
     }
+        //PRIKLADY 1
+        static void uloha1() {
+            System.out.println("Hello World");
+        }
+
+        static int uloha2(int x,int y) {
+            if (x > y) {
+                return 1;
+            }
+            else if (x == y) {
+                return 0;
+            }
+            else{
+                return -1;
+            }
+        }
+
+        //PRIKLADY 2
+
+
 }
